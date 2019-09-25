a9d8f5337434cb1ea949cbc81befb56d71a96bbe
Nicholas Jansen <000395661@mohawkcollege.ca>
2019-09-25 05:30:53 -0400

    Removed dumb stuff

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -16,10 +16,6 @@ class Profile extends Component {
                     <div className="col-s-12 justify-content-center">
                         <h4>
                             <b>Hey there,</b> {user.name.split(" ")[0]}
-                            <p className="flow-text grey-text text-darken-1">
-                                You are logged into a full-stack{" "}
-                                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
                         </h4>
                         <button
                             style={{
